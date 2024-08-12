393855fb1f31d3bdabefc49b64481549927f29df
HelenaCanever <49685784+HelenaCanever@users.noreply.github.com>
2024-08-11 19:34:13 -0500

    removed sentitive files

--- data_pipeline.bat
+++
@@ -1,4 +0,0 @@
-cd C:\Users\helen\Desktop\Coding\Repos\kesamokki_project
-call myenv\Scripts\activate.bat
-set PYTHONPATH=C:\Users\helen\Desktop\Coding\Repos\kesamokki_project
-python scripts\run_data_pipeline.py
